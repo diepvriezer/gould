fe0a8bcc7e1bb4801ecb8bb6f67840321fcad7a0
Floris den Heijer <floris@denheijer.com>
2015-08-07 21:12:31 +0200

    Again with the updates

--- final_report/samples/walkabout.cs
+++ final_report/samples/walkabout.cs
@@ -1,12 +1,12 @@
-class Walkabout
+public abstract class Walkabout
 {
-	void visit(Object v)
+	public void Visit(Object v)
 	{
 		if ( v != null )
 			if (/* this has a public visit method for the class of v */)
-				this.visit(v);
+				this.Visit(v);
 			else
 				for (each field f of v)
-					this.visit(v.f);
+					this.Visit(v.f);
 	}
 }
